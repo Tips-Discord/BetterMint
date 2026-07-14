cfc7a229ac6b1ea9288d2a88ee086b96bbbed304
Tips <tips94988@gmail.com>
2026-07-14 18:53:45 +0200

    Export materialValues for external use

--- src/position.ts
+++ src/position.ts
@@ -12,7 +12,7 @@ const inEco = (fen: string): boolean => {
     return ecoSet.has(fen);
 };
 
-const materialValues: Record<string, number> = {
+export const materialValues: Record<string, number> = {
     p: 1, n: 3, b: 3, r: 5, q: 9,
     P: 1, N: 3, B: 3, R: 5, Q: 9,
 };
